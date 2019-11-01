5d5bf49e4ebb73c3e7a7bcd7d146def3ca895a4a
Justin Le <justin@jle.im>
2019-10-31 17:19:20 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const child_process = require('child_process');
+const fs = require('fs');
+
+try {
+  ro = core.getInput('runs-on').split("-")[0];
+
+  switch(ro) {
+
+    case "ubuntu":
+
+      const pkgs       = core.getInput('aptitude');
+      const mgr        = "apt-get"
+      const installcmd = "sudo apt-get install"
+      break;
+
+    case "macOS":
+      const pkgs       = core.getInput('brew');
+      const mgr        = "brew"
+      const installcmd = "brew install"
+      child_process.execSync("brew update");
+      break;
+
+    default:
+      core.setFailed("Unsopported OS");
+  }
+
+  child_process.execSync(installcmd + " " + pkgs);
+
+} catch (error) {
+    core.setFailed(error.message);
+}
+
