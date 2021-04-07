1bd986673a11f968d816038cc675fccec9532242
Wessel van Norel <wessel.van.norel@delgurth.com>
2021-04-07 02:14:23 +0200

    - add snap - add linuxbrew - remove unused dependencies - upgrade dependency dependabot complained about

--- README.md
+++ README.md
@@ -6,12 +6,13 @@ Download and install packages using OS package manager
 Usage:
 
 ```yaml
-use: mstksg/get-package@v1
+use: delgurth/get-package@v4
 with:
   brew: brewdep1 brewdep2
   apt-get: aptdep1 aptdep2
+  snap: snapdep1 snapdep2
+  linuxbrew: linuxbrewdep1 linuxbrewdep2
 ```
 
 The appropriate package manager will be run on the appropriate OS: `brew` field
-will be ignored for `ubuntu` OS, for instance.
-
+will be ignored for a `linux` OS, for instance.
--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 const os = require('os')
 const core = require('@actions/core');
-const github = require('@actions/github');
 const child_process = require('child_process');
-const fs = require('fs');
 const commandExists = require('command-exists');
 
 const managers = {
   "apt-get": {
-      "check": "apt-get",
-      "platform": "linux",
-      "command": "sudo apt-get update && sudo apt-get -y install"
-    },
+    "check": "apt-get",
+    "platform": "linux",
+    "command": "sudo apt-get update && sudo apt-get -y install"
+  },
   "brew": {
-      "check": "brew",
-      "platform": "darwin",
-      "command": "brew update && brew install"
-    }
+    "check": "brew",
+    "platform": "darwin",
+    "command": "brew update && brew install"
+  },
+  "snap": {
+    "check": "snap",
+    "platform": "linux",
+    "command": "sudo snap install"
+  },
+  "linuxbrew": {
+    "check": "brew",
+    "platform": "linux",
+    "command": "brew update && brew install"
+  }
 };
 
 try {
 
-  var pkgs;
+  let pkgs;
 
   for (let [mgr, info] of Object.entries(managers)) {
     if (commandExists.sync(info.check) && os.platform() === info.platform) {
@@ -32,6 +40,6 @@ try {
   }
 
 } catch (error) {
-    core.setFailed(error.message);
+  core.setFailed(error.message);
 }
 
